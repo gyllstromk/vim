f2b8ddb43dca41dd0e477896824fcd2fda50b27f
Karl Gyllstrom <karl.gyllstrom@gmail.com>
2023-02-03 08:05:17 -0800

    comments working

--- lua/deps.lua
+++ lua/deps.lua
@@ -3,74 +3,78 @@ vim.cmd [[packadd packer.nvim]]
 return require("packer").startup {
   {
     { "wbthomason/packer.nvim", opt = true } ,
-    "gpanders/editorconfig.nvim" ,
-    "AndrewRadev/splitjoin.vim" ,
-    "machakann/vim-sandwich" ,
-    "tweekmonster/startuptime.vim" ,
-    "elianiva/gitgud.nvim" ,
+    --"gpanders/editorconfig.nvim" ,
+    --"AndrewRadev/splitjoin.vim" ,
+    --"machakann/vim-sandwich" ,
+    -- "tweekmonster/startuptime.vim" ,
+    -- "elianiva/gitgud.nvim" ,
 
-    "Mofiqul/vscode.nvim",
+    -- "Mofiqul/vscode.nvim",
 
-    "feline-nvim/feline.nvim",
+    -- "feline-nvim/feline.nvim",
 
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "windwp/nvim-ts-autotag",
+    -- "nvim-treesitter/nvim-treesitter",
+    -- "nvim-treesitter/nvim-treesitter-textobjects",
+    -- "windwp/nvim-ts-autotag",
 
-    'numToStr/Comment.nvim',
     {
-      require('Comment').setup({
-        toggler = {
-          line = '[cn',
-          block = 'gbc',
-        },
-        opleader = {
-          line = '[cn',
-          -- block = '[',
-        },
-        mappings = {
-          basic = true,
-          extra = true,
-        },
-      }),
+      "numToStr/Comment.nvim",
+      config = function()
+          require('Comment').setup(
+          {
+            opleader = {
+              line = '<leader>cn',
+            },
+            toggler = {
+              line = '<leader>cn',
+            },
+            mappings = {
+              basic = true,
+              extra = false,
+            },
+          })
+      end,
     },
-    "JoosepAlviste/nvim-ts-context-commentstring",
+    -- "JoosepAlviste/nvim-ts-context-commentstring",
 
-    "nvim-telescope/telescope.nvim",
-    "nvim-lua/plenary.nvim",
-    "nvim-lua/popup.nvim",
-    "kyazdani42/nvim-web-devicons",
-    "tami5/sqlite.lua",
-    { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
-    "nvim-telescope/telescope-ui-select.nvim",
+    -- "nvim-telescope/telescope.nvim",
+    -- "nvim-lua/plenary.nvim",
+    -- "nvim-lua/popup.nvim",
+    -- "kyazdani42/nvim-web-devicons",
+    -- "tami5/sqlite.lua",
+    -- { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
+    -- "nvim-telescope/telescope-ui-select.nvim",
 
     -- requires plenary.nvim
-    "ruifm/gitlinker.nvim",
+    -- {
+    --   "ruifm/gitlinker.nvim",
+    --   requires={"nvim-lua/plenary.nvim"},
+    -- }
 
-    "https://gitlab.com/yorickpeterse/nvim-pqf",
+    -- "https://gitlab.com/yorickpeterse/nvim-pqf",
+    --
+    -- "junegunn/vim-easy-align",
+    --
+    -- "mhinz/vim-sayonara",
 
-    "junegunn/vim-easy-align",
-
-    "mhinz/vim-sayonara",
-
-    { "nvim-neo-tree/neo-tree.nvim", branch = "v2.x" },
-    "MunifTanjim/nui.nvim",
+    -- { "nvim-neo-tree/neo-tree.nvim", branch = "v2.x" },
+    -- "MunifTanjim/nui.nvim",
 
     "hrsh7th/nvim-cmp",
     -- "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-vsnip",
-    "hrsh7th/vim-vsnip",
+    -- "hrsh7th/cmp-path",
+    -- "hrsh7th/cmp-buffer",
+    -- "hrsh7th/cmp-vsnip",
+    -- "hrsh7th/vim-vsnip",
     "ellisonleao/gruvbox.nvim",
 
     -- requires plenary.nvim
-    "lewis6991/gitsigns.nvim",
+    -- "lewis6991/gitsigns.nvim",
 
     -- requires kyazdani42/nvim-web-devicons
-    "akinsho/nvim-bufferline.lua",
+    -- "akinsho/nvim-bufferline.lua",
 
-    "windwp/nvim-autopairs",
+    -- "windwp/nvim-autopairs",
 
     -- "neovim/nvim-lspconfig",
     -- "jose-elias-alvarez/null-ls.nvim",
@@ -81,45 +85,42 @@ return require("packer").startup {
     -- "rcarriga/nvim-dap-ui",
     -- "leoluz/nvim-dap-go",
 
-    "phaazon/hop.nvim",
+    -- "phaazon/hop.nvim",
 
     -- "folke/zen-mode.nvim",
 
-    "andymass/vim-matchup",
+    -- "andymass/vim-matchup",
 
     -- "vim-test/vim-test",
 
     "norcalli/nvim-colorizer.lua",
-'altercation/vim-colors-solarized',
-'bronson/vim-trailing-whitespace',
-'ConradIrwin/vim-bracketed-paste',
-'easymotion/vim-easymotion',
-'hhvm/vim-hack',
-'phleet/vim-mercenary',
-'jlanzarotta/bufexplorer',
-'junegunn/vim-easy-align',
-'ludovicchabant/vim-lawrencium',
-'mileszs/ack.vim',
-'nvie/vim-flake8',
-'pdavydov108/vim-lsp-cquery',
-'Shougo/vimproc.vim',
-'scrooloose/nerdcommenter',
-'prabirshrestha/asyncomplete.vim',
-'prabirshrestha/async.vim',
-'prabirshrestha/vim-lsp',
-'prabirshrestha/asyncomplete-lsp.vim',
-'Shougo/unite.vim',
-'tpope/vim-abolish',
-'tpope/vim-fugitive',
-'vim-scripts/a.vim',
-'junegunn/fzf',
-'Shougo/denite.nvim',
---Plug 'Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
+    -- "bronson/vim-trailing-whitespace",
+    -- "ConradIrwin/vim-bracketed-paste",
+    -- "easymotion/vim-easymotion",
+    -- "hhvm/vim-hack",
+    "jlanzarotta/bufexplorer",
+    -- "junegunn/vim-easy-align",
+    "ludovicchabant/vim-lawrencium",
+    -- "mileszs/ack.vim",
+    -- "nvie/vim-flake8",
+    -- "pdavydov108/vim-lsp-cquery",
+    -- "Shougo/vimproc.vim",
+    -- "prabirshrestha/asyncomplete.vim",
+    -- "prabirshrestha/async.vim",
+    -- "prabirshrestha/vim-lsp",
+    -- "prabirshrestha/asyncomplete-lsp.vim",
+    -- "Shougo/unite.vim",
+    --"tpope/vim-abolish",
+    "tpope/vim-fugitive",
+    "vim-scripts/a.vim",
+    -- "junegunn/fzf",
+    -- "Shougo/denite.nvim",
+--Plug "Shougo/deoplete.nvim', { 'do': ':UpdateRemotePlugins' }
 
-'Shougo/echodoc.vim',
-'neovim/nvim-lspconfig',
-'jose-elias-alvarez/null-ls.nvim',
-'nvim-treesitter/nvim-treesitter',
-'nvim-telescope/telescope.nvim',
+    -- 'Shougo/echodoc.vim',
+    -- 'neovim/nvim-lspconfig',
+    -- 'jose-elias-alvarez/null-ls.nvim',
+    -- 'nvim-treesitter/nvim-treesitter',
+    -- 'nvim-telescope/telescope.nvim',
   }
 }
