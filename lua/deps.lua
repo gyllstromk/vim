e5b4b6c2b070c1f6c6906cc2334d8336f040a3df
gylls <gylls@fb.com>
2023-08-23 09:49:54 -0700

    auto pair setup

--- lua/deps.lua
+++ lua/deps.lua
@@ -81,7 +81,20 @@ local pkg = require("packer").startup {
     -- requires kyazdani42/nvim-web-devicons
     -- "akinsho/nvim-bufferline.lua",
 
-    -- "windwp/nvim-autopairs",
+    {
+      "windwp/nvim-autopairs",
+      after = "nvim-cmp",
+      config = function()
+        require("nvim-autopairs").setup({})
+
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        local cmp = require("cmp")
+        cmp.event:on(
+          "confirm_done",
+          cmp_autopairs.on_confirm_done({ map_char = { tex = "" } })
+        )
+      end,
+    },
 
     "jose-elias-alvarez/null-ls.nvim",
     -- "jose-elias-alvarez/nvim-lsp-ts-utils",
