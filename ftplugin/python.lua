049d71c9ab6a372b5ff38ee5a2f6404a7641abb6
gylls <gylls@fb.com>
2024-10-02 07:24:26 -0700

    python commands

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -5,6 +5,8 @@ map("i", "<leader>pr", "print()i", { noremap = true })
 map("i", "<leader>pf", "print(f\"\")hi", { noremap = true })
 map("i", "<leader>if", "if :i", { noremap = true })
 map("i", "<C-o>", "o", { noremap = true })
+map("i", "<leader>lx", "logging.exception()i", { noremap = true })
+map("i", "<leader>im", "import ", { noremap = true })
 
 map("n", "<leader>o", "wbiOptional[ea]", { noremap = true })
 
