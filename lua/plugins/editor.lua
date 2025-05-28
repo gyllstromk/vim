6c7292a30d8301826eb84ab9fc22398fafd5118c
gylls <gylls@fb.com>
2025-05-28 08:40:29 -0700

    fzf extension

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -72,7 +72,7 @@ return {
       })
     end,
   },
-
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
@@ -103,7 +103,16 @@ return {
             },
           },
         },
+        extensions = {
+          fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = true,  -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+          }
+        },
       })
+      require("telescope").load_extension("fzf")
     end,
   },
   {
