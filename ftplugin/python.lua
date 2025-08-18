015d4713fab8a1683336b08dceeac83ed3136bc7
gylls <gylls@fb.com>
2025-08-18 08:50:22 -0700

    python logging

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -3,9 +3,13 @@ local map = vim.api.nvim_set_keymap
 
 map("i", "<leader>pr", "print()i", { noremap = true })
 map("i", "<leader>pf", "print(f\"\")hi", { noremap = true })
+map("i", "<leader>li", "logging.info()i", { noremap = true })
+map("i", "<leader>lx", "logging.exception()i", { noremap = true })
+map("i", "<leader>lw", "logging.warning()i", { noremap = true })
+
 map("i", "<leader>if", "if :i", { noremap = true })
 map("i", "<C-o>", "o", { noremap = true })
-map("i", "<leader>lx", "logging.exception()i", { noremap = true })
+
 map("i", "<leader>im", "import ", { noremap = true })
 
 map("n", "<leader>o", "wbiOptional[ea]", { noremap = true })
